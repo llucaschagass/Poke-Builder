3b86159a47f68a3267949761eb299f98a7d314d4
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-04-06 11:37:59 -0300

    first commit

---
+++ src/main/java/com/teambuilder/pokebuilder/PokebuilderApplication.java
@@ -0,0 +1,13 @@
+package com.teambuilder.pokebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokebuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teambuilder/pokebuilder/PokebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambuilder.pokebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
