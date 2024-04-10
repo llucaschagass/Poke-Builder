ca8fc96d3a3305117c0ffcf3e819edc44ef2a26a
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-04-10 14:29:48 -0300

    bug: resolvendo conflitos de arquivos

--- src/main/java/com/teambuilder/pokebuilder/controllers/PokeBuilderController.java
+++ src/main/java/com/teambuilder/pokebuilder/controllers/PokeBuilderController.java
@@ -1,17 +1,18 @@
 package com.teambuilder.pokebuilder.controllers;
 
 import com.teambuilder.pokebuilder.models.Pokemon;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class PokeBuilderController {
 
     @GetMapping("/team")
-    public List<Pokemon> getTeam() {
+    public String getTeam(Model model) {
         List<Pokemon> team = new ArrayList<>();
 
         // Criando instâncias dos meus Pokémon favoritos
@@ -30,6 +31,10 @@ public class PokeBuilderController {
         team.add(raichu);
         team.add(lucario);
 
-        return team;
+        // Adicionando a lista de Pokémon ao modelo
+        model.addAttribute("team", team);
+
+        // Retorna o nome da view
+        return "team";
     }
 }
